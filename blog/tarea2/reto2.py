4759f3f0e8d5652b69c428f85a8bbcb1f392e8a8
juansanchezlon <juan.sanchezl@est.iudigital.edu.co>
2025-11-22 15:38:16 -0500

    Create reto2.py

---
+++ blog/tarea2/reto2.py
@@ -0,0 +1,11 @@
+# Reto 2 - Tortuga bajando
+
+#Simula el rastro de una tortuga moviéndose hacia abajo usando solo texto.
+
+print("Creando tortuga que baja...")
+pasos = int(input("¿Cuántos pasos baja la tortuga? "))
+for i in range(pasos):
+    print("|")
+print("V")
+
+#El programa imprime una columna (|) para cada paso y al final una (V) indicando la dirección hacia abajo.

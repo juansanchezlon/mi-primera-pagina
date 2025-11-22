243860d43abb9290291b5eb3bd571137289dee18
juansanchezlon <juan.sanchezl@est.iudigital.edu.co>
2025-11-22 15:36:41 -0500

    Create reto1.py

---
+++ blog/tarea2/reto1.py
@@ -0,0 +1,10 @@
+# Reto 1 - Tortuga simple
+
+#Enunciado: Simula el movimiento de una tortuga hacia adelante usando únicamente texto.
+
+print("Creando una tortuga simulada...")
+pasos = int(input("¿Cuántos pasos debe avanzar? "))
+print("La tortuga avanza... de a 1 paso.")
+print("." * pasos)
+
+#El programa pide un número de pasos y dibuja una línea de puntos representando el movimiento.
